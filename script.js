92ed2374030e1d88deb5b1836792c091b76cf12b
Jeva Varshini S <jevavarshini54@gamil.com>
2026-04-16 12:19:20 +0530

    <dialog> created and connected to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPopup(type) {
+  const dialog = document.getElementById('popup-dialog');
+  const title = document.getElementById('popup-title');
+  const details = document.getElementById('popup-details');
+  const table = document.getElementById('popup-table');
+        
+  }
